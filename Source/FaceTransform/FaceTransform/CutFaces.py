c70c3d1f6cb2d8b3b9fc8f1f8fee3fc5f009feb4
KirillovDenis <pushistiy96@mail.ru>
2016-10-29 15:20:35 +0300

    com2

--- Source/FaceTransform/FaceTransform/CutFaces.py
+++ Source/FaceTransform/FaceTransform/CutFaces.py
@@ -1,7 +1,16 @@
 import FaceTransform as FT
+import numpy as np
 import json
 import cv2
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+#
+def cutFaceWB(imageName,jsonDescription):
 
+    listCoordFace=getCoordinateFace(jsonDescription)
+    img=cv2.imread(imageName,0)
+    cutImg=img[listCoordFace[0]:listCoordFace[2],listCoordFace[1]:listCoordFace[3]]
+    return cutImg
 #cutFace return matrix 3-demention cut face from base picture
 def cutFace(imageName,jsonDescription):
 
@@ -19,7 +28,17 @@ def getCoordinateFace(jsonDescrip):
    
     return outListCoordinateFace
 
+def getFunction():
+    
+    tmp=cutFaceWB('kor.jpg',open('jsonDesc.txt','r'))
+    vecttmp=tmp.flatten()
+    f=interp1d(np.arange(len(vecttmp)),vecttmp)
+    plt.plot(np.arange(len(vecttmp)),vecttmp)
+    plt.show()
+    #cv2.imshow('image',tmp)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
+    pass
 
-tmp =  FT.GetThreeDimMatrix(cv2.imread('kor.jpg'))
-print(tmp)
+getFunction()
