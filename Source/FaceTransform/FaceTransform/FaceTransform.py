ccb4f2e3275ad91f2b3bee22fe8309ceff12716d
inventive-seven <mr.kakorin@yandex.ru>
2016-10-30 01:36:30 +0300

    int

--- Source/FaceTransform/FaceTransform/FaceTransform.py
+++ Source/FaceTransform/FaceTransform/FaceTransform.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import CutFaces as cf
 import json
-
+import math
 #convert 3-chaneles image to 1-channel mat summed from 3-channels by mod 255 
 def GetTwoDimMatrix(img):
 	height, width, channels = img.shape	
@@ -49,11 +49,59 @@ def TransformMarkCoordinates(markcoord,facecoord):
 def InterpolateBetween(img,ec):
     k=2;
     for i in range(0,len(ec)):
-                img[ec[i][0],ec[i][1]] = img[ec[i][0]-k,ec[i][1]]/3+img[ec[i][0],ec[i][1]-k]/3+img[ec[i][0]-k,ec[i][1]-k]/3
+        img[ec[i][0],ec[i][1]] = img[ec[i][0]-k,ec[i][1]]/3+img[ec[i][0],ec[i][1]-k]/3+img[ec[i][0]-k,ec[i][1]-k]/3
                 #print(ec[i][0],ec[i][1])      
+    for i in range(0,len(ec)):              
+        if img[ec[i][0],ec[i][1],0]<50:
+           sum =0;
+           for j in range(1,9):
+               sum = sum + img[ec[i][0]-j,ec[i][1],0]/24 + img[ec[i][0],ec[i][1]-j,0]/24 + img[ec[i][0]-j,ec[i][1]-j,0]/24
+           if sum<60:
+                sum=sum*math.log(sum,2);
+           #print(sum)
+           img[ec[i][0],ec[i][1],0]= sum
+        if img[ec[i][0],ec[i][1],1]<50:
+           sum =0;
+           for j in range(1,9):
+                sum = sum + img[ec[i][0]-j,ec[i][1],1]/24 + img[ec[i][0],ec[i][1]-j,1]/24 + img[ec[i][0]-j,ec[i][1]-j,1]/24
+           if sum<60:
+               sum=sum*math.log(sum,2);
+           #print(sum)
+           img[ec[i][0],ec[i][1],1]= sum
+        if img[ec[i][0],ec[i][1],2]<50:
+           sum =0;
+           for j in range(1,9):
+                sum = sum + img[ec[i][0]-j,ec[i][1],2]/24 + img[ec[i][0],ec[i][1]-j,2]/24 + img[ec[i][0]-j,ec[i][1]-j,2]/24
+           if sum<60:
+                sum=sum*math.log(sum,2);
+                #print(sum)
+           img[ec[i][0],ec[i][1],2]= sum
+        print(img[ec[i][0],ec[i][1]])
+
+    return
+
+def InterpolateBetween2(img,ec):
+    k=2;
     for i in range(0,len(ec)):
-        if img[ec[i][0],ec[i][1]].any()==0:
-            print(ec[i][0],ec[i][1])
+        img[ec[i][0],ec[i][1]] = img[120,120]#img[ec[i][0]-k,ec[i][1]]/3+img[ec[i][0],ec[i][1]-k]/3+img[ec[i][0]-k,ec[i][1]-k]/3
+                #print(ec[i][0],ec[i][1])      
+   
+
+    return
+
+def sglazh(img):
+    h,w,d=img.shape
+    k=2
+    p=50
+    for i in range(h):
+        for j in range(w):
+            if abs(img[i,j,0] - img[i-1,j,0])>p and abs(img[i,j,0] - img[i,j-1,0])> p and abs(img[i,j,0] - img[i-1,j-1,0])> p:
+                img[i,j,0] = img[i-k,j,0]/3+img[i,j-k,0]/3+img[i-k,j-k,0]/3
+            if abs(img[i,j,1] - img[i-1,j,1])>p and abs(img[i,j,1] - img[i,j-1,1])>p and abs(img[i,j,1] - img[i-1,j-1,1])>p:
+                img[i,j,1] = img[i-k,j,1]/3+img[i,j-k,1]/3+img[i-k,j-k,1]/3
+            if abs(img[i,j,2] - img[i-1,j,2])>p and abs(img[i,j,2] - img[i,j-1,2])>p and abs(img[i,j,2] - img[i-1,j-1,2])>p:
+                img[i,j,2] = img[i-k,j,2]/3+img[i,j-k,2]/3+img[i-k,j-k,2]/3
+
     return
 
 def GetIndexesArr(arr):
