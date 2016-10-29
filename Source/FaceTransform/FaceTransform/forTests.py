ccf58dd9ddadc5b640d732b2f2c204b41aac8b46
KirillovDenis <pushistiy96@mail.ru>
2016-10-29 16:43:41 +0300

    engine5

--- Source/FaceTransform/FaceTransform/forTests.py
+++ Source/FaceTransform/FaceTransform/forTests.py
@@ -1,22 +1,6 @@
 import CutFaces as cf
 import FaceTransform as ft
-import matplotlib.pyplot as plt
-from pandas import date_range,Series,DataFrame,read_csv, qcut
-from pandas.tools.plotting import radviz,scatter_matrix,bootstrap_plot,parallel_coordinates
-from numpy.random import randn
-from pylab import *
 
-from matplotlib import rcParams
-#testImFace=cf.cutFace('kor.jpg','jsonDesc.txt')
-#testWB=ft.GetGrayScaleImg(testImFace)
-#myfun=cf.getFunctionFromMatixWhiteBlack(testWB)
-
-
-x = linspace(0, 5, 10)
-y = x ** 2
-plt.figure()
-plt.plot(x, y, 'r')
-xlabel('x')
-ylabel('y')
-title('title')
-plt.show()
+testImFace=cf.cutFace('kor.jpg','jsonDesc.txt')
+testWB=ft.GetGrayScaleImg(testImFace)
+myfun=cf.getFunctionFromMatixWhiteBlack(testWB)
