a47ece8ba5ae7f89428b4b874faf99e981138ea4
KirillovDenis <pushistiy96@mail.ru>
2016-10-29 16:15:59 +0300

    engine2

--- Source/FaceTransform/FaceTransform/FS_engine.py
+++ Source/FaceTransform/FaceTransform/FS_engine.py
@@ -1,3 +1,7 @@
 import sys
-inputImage=sys.argv[0]
-inputJSON=sys.argv[1]
+if len(sys.argv)>1:
+    inputImage=sys.argv[1]
+    inputJSON=sys.argv[0]
+    print(inputImage)
+else:
+    print('что-то пошло не так')
--- Source/FaceTransform/FaceTransform/forTests.py
+++ Source/FaceTransform/FaceTransform/forTests.py
@@ -1,6 +1,12 @@
 import CutFaces as cf
 import FaceTransform as ft
 import matplotlib.pyplot as plt
+from pandas import date_range,Series,DataFrame,read_csv, qcut
+from pandas.tools.plotting import radviz,scatter_matrix,bootstrap_plot,parallel_coordinates
+from numpy.random import randn
+from pylab import *
+
+from matplotlib import rcParams
 #testImFace=cf.cutFace('kor.jpg','jsonDesc.txt')
 #testWB=ft.GetGrayScaleImg(testImFace)
 #myfun=cf.getFunctionFromMatixWhiteBlack(testWB)
