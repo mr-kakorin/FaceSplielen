61d644b068125f9e18bf7cdc453208bc8352a592
KirillovDenis <pushistiy96@mail.ru>
2016-10-30 00:57:54 +0300

    goodfile

--- Source/FaceTransform/FaceTransform/FS_engine.py
+++ Source/FaceTransform/FaceTransform/FS_engine.py
@@ -5,18 +5,19 @@ import os
 import CutFaces as cf
 if len(sys.argv)>1:
     inputID=sys.argv[1]
-    inputImageName=os.path.abspath('FS_engine.py')+'/../../../../Destination/uploads/'+inputID
-    inputJSONName=os.path.abspath('FS_engine.py')+'/../../../../Destination/json/'+inputID
+    inputImageName=__file__+'/../../../../Destination/uploads/'+inputID
+    inputJSONName=__file__+'/../../../../Destination/json/'+inputID
     print('success')
     cutImage=cf.cutFace(inputImageName,inputJSONName)
 
-    outFile=open(os.path.abspath('FS_engine.py')+'/../../../../Destination/results/'+inputID,'w')
+    outFile=open(__file__+'/../../../../Destination/results/'+inputID,'w')
     outFile.write(json.dumps(cutImage.flatten()))
     outFile.close()
     #cv2.imwrite('../../../Destination/results/'+inputID,cutImage)
 else:
-    outFile=open(os.path.abspath('FS_engine.py')+'/../../../../Destination/results/','w')
-    outFile.close()
+    print(__file__)
+    #outFile=open(os.path.abspath('FS_engine.py')+'/../../../../Destination/results/','w')
+    #outFile.close()
     #print(os.path.abspath('FS_engine.py')+'/../../../Destination/uploads/')
     #close(outFile)
     #cutImage=cf.cutFace('../../../Destination/uploads/kor.jpg','jsonDesc.txt')
