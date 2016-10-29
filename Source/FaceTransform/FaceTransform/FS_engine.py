4570cf6aa6a61b704dc08520e62bd77cb0fac64a
KirillovDenis <pushistiy96@mail.ru>
2016-10-30 00:30:45 +0300

    fiiiiix

--- Source/FaceTransform/FaceTransform/FS_engine.py
+++ Source/FaceTransform/FaceTransform/FS_engine.py
@@ -5,12 +5,12 @@ import os
 import CutFaces as cf
 if len(sys.argv)>1:
     inputID=sys.argv[1]
-    inputImageName=os.path.abspath('FS_engine,py')+'../../../Destination/uploads/'+inputID
-    inputJSONName=os.path.abspath('FS_engine,py')+'../../../Destination/json/'+inputID
+    inputImageName=os.path.abspath('FS_engine.py')+'/../../../Destination/uploads/'+inputID
+    inputJSONName=os.path.abspath('FS_engine.py')+'/../../../Destination/json/'+inputID
     print('success')
     cutImage=cf.cutFace(inputImageName,inputJSONName)
 
-    outFile=open(os.path.abspath('FS_engine,py')+'../../../Destination/results/'+inputID,'w')
+    outFile=open(os.path.abspath('FS_engine.py')+'/../../../Destination/results/'+inputID,'w')
     outFile.write(json.dumps(cutImage.flatten()))
     outFile.close()
     #cv2.imwrite('../../../Destination/results/'+inputID,cutImage)
