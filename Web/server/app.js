91af1457aa489715d9abf91bb570e8bb21aa2f0a
ekorolev <aksyline@gmail.com>
2016-10-29 15:33:44 +0300

    webApp

---
+++ Web/server/app.js
@@ -0,0 +1,41 @@
+var app = require('express')();
+var path = require('path');
+var upload = require('multer')({
+	dest: path.resolve( '../../../Destination/uploads' ),
+	limits: {
+		fileSize: 2*1024*1024
+	}
+});
+var vision = require('@google-cloud/vision')({
+	projectId: 'facespielen',
+	keyFilename: path.resolve('keyfile.json')
+});
+
+app.get( '/' , function ( req , res ) {
+	res.sendFile( path.resolve( '../public/index.html' ) );
+});
+
+app.post ( '/run', upload.single("photo"), function ( req, res ) {
+
+	console.log('Загрузка файла: ', req.file.path);
+
+	vision.detectFaces( 
+		path.resolve( '../public/uploads/'+req.file.filename ),
+		function ( err, faces ) {
+			if (err) {
+				console.log('Ошибка определения лиц: ', err);
+			} else {
+
+				console.log('Лица определены: ', faces);
+
+				res.send({
+					success: true
+				})
+			}
+		}
+	);
+});
+
+app.listen( 8097, function () {
+	console.log( 'Server started!' );
+});
