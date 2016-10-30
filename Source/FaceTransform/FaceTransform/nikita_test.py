4ebb90ed5cbec27a85539f859c46e18eb2359ade
inventive-seven <mr.kakorin@yandex.ru>
2016-10-30 06:50:10 +0300

    dd

--- Source/FaceTransform/FaceTransform/nikita_test.py
+++ Source/FaceTransform/FaceTransform/nikita_test.py
@@ -13,6 +13,7 @@ img = cv2.imread('kor.jpg');
 
 imgf,fcoord = cf.cutFace('kor.jpg',json);
 
+print(fcoord)
 imgf2 = cv2.copyMakeBorder(imgf,0,0,0,0,cv2.BORDER_REPLICATE)
 
 imgg, l, b = cf.rot(imgf,[100,80],31,41,-25,True);
