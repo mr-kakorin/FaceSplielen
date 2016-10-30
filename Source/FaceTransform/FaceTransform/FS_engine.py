30227068cd5fbd1094f5b43fa92b86c60f2e81a4
ekorolev <aksyline@gmail.com>
2016-10-30 06:24:55 +0300

    asd

--- Source/FaceTransform/FaceTransform/FS_engine.py
+++ Source/FaceTransform/FaceTransform/FS_engine.py
@@ -11,7 +11,7 @@ if len(sys.argv)>1:
     inputJSONName=os.path.abspath(__file__+'/../../../../Destination/json/'+inputID)
     print('success')
     cutImage, tmp=cf.cutFace(inputImageName,inputJSONName)
-    cutImage = tf.GetGrayScaleImg(cutImage);
+    cutImage = tf.GetTwoDimMatrix(cutImage);
     cutmage = cv2.resize(cutImage,None,fx=0.1, fy=0.1, interpolation = cv2.INTER_CUBIC)
 
     with open(inputID, 'wb') as outfile:
