419e871cc82de19ad3a8725f4afb247f5a4f17ae
ekorolev <aksyline@gmail.com>
2016-10-30 06:16:44 +0300

    asd

--- Source/FaceTransform/FaceTransform/FS_engine.py
+++ Source/FaceTransform/FaceTransform/FS_engine.py
@@ -10,6 +10,8 @@ if len(sys.argv)>1:
     inputJSONName=os.path.abspath(__file__+'/../../../../Destination/json/'+inputID)
     print('success')
     cutImage, tmp=cf.cutFace(inputImageName,inputJSONName)
+    cutImage = GetGrayScaleImg(cutImage);
+    cutmage = cv2.resize(cutImage,None,fx=0.1, fy=0.1, interpolation = cv2.INTER_CUBIC)
 
     with open(inputID, 'wb') as outfile:
         outjson = {}
